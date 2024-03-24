3c3cc454e0e6be30ed9d646ca2769bce5b4e6797
MIDO <72321805+Mahmoud-U@users.noreply.github.com>
2024-03-25 00:42:05 +0200

    Crazy_Modifications

--- Bank_System.h
+++ Bank_System.h
@@ -3,76 +3,114 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <regex>
 using namespace std;
 class Bank_System
 {
+
 };
-class Person {
+class Person 
+{
 protected:
+	//Attributes
 	int id;
 	string name;
 	string password;
 public:
-	Person() 
+	//Constructors
+	Person()
 	{
 		id = 0;
 	}
 	Person(int id, string name, string password) {
 		this->id = id;
-		if (name.size() > 5 && name.size() < 20) {
+
+		if (Valid_Password(password) && Valid_Name(name)) {
 			this->name = name;
-		}
-		for (int i = 0; i < password.size(); i++) {
-			if ((i >= 'a' && i <= 'z') && (password.size() > 8 && password.size() < 20)) {
-				this->password = password;
-			}
+			this->password = password;
 		}
 	}
-	//setters
-	void setId(int id) {
+
+	//Name & Password Validation
+	bool Valid_Name(string& name)
+	{
+		regex pattern("^[A-Z][a-z]{4,19}$");
+
+		return regex_match(name, pattern);
+	}
+
+	bool Valid_Password(string& password)
+	{
+		regex pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z0-9]{8,20}$");
+
+		return regex_match(password, pattern);
+	}
+	
+	//setters:
+	void setId(int id) 
+	{
 		this->id = id;
 	}
-	void setName(string name) {
+	void setName(string name) 
+	{
 		this->name = name;
 	}
-	void setPassword(string password) {
+	void setPassword(string password)
+	{
 		this->password = password;
 	}
-	int getId() {
+
+	//Getters
+	int getId() 
+	{
 		return this->id;
 	}
-	string getName() {
+	string getName() 
+	{
 		return this->name;
 	}
-	string getPassword() {
+	string getPassword() 
+	{
 		return this->password;
 	}
-	/*virtual Person display() = 0;*/
 };
-class Client : public Person {
+
+class Client : public Person 
+{
 private:
+	//Attributes
 	double balance;
 public:
+	//Constructors
 	Client() {
 		balance = 1500.0;
 	}
-	Client(int id, string name, string password, double balance) :Person(id, name, password) {
+	Client(int id, string name, string password, double balance)
+			:Person(id, name, password)
+	{
 		this->balance = balance;
+	}
 
-	}
-	void setBalance(double balance) {
+	//Setters
+	void setBalance(double balance)
+	{
 		this->balance = balance;
 	}
-	double getBalance() {
+	double getBalance() 
+	{
 		return balance;
 	}
-	//deposite
-	double deposite(double amount) {
+
+	//Deposite
+	double deposite(double amount) 
+	{
 		balance += amount;
 		return balance;
 	}
-	//withdraw
-	double withdraw(double amount) {
+
+	//Withdraw
+	double withdraw(double amount) 
+	{
 		if (amount <= balance)
 			balance -= amount;
 		else
@@ -83,14 +121,17 @@ public:
 	//TransferTo
 	double transferTo(double amount, Client& x) 
 	{
-		if (amount <= balance) {
-			balance -= balance;
+		if (amount <= balance) 
+		{
+			balance -= amount;
 			x.deposite(amount);
 		}
 		else
+		{
 			cout << "Amount Exceed" << endl;
+		}
+		return balance;
 	}
-
 
 	Person* display() {
 		return this;
