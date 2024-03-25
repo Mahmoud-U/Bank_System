273f98df347d6305b54f8ccc85ea41667710d8d0
MIDO <72321805+Mahmoud-U@users.noreply.github.com>
2024-03-25 03:58:13 +0200

    Crazy_mods 2

--- Bank System.cpp
+++ Bank System.cpp
@@ -6,16 +6,37 @@
 using namespace std;
 int main()
 {
-	//testing
-	Person p(123, "m12", "a555");
-	Client p1, p2;
-	cout << "name" << " " << p.getName() << endl;
-	cout << "id: " << p.getId() << endl;
-	cout << "pwd: " << p.getPassword() << endl;
-	p2.transferTo(1000.00, p1);
-	cout << p2.deposite(5000) << endl;
-	cout << p2.withdraw(1000) << endl;
-	cout << p2.transferTo(3000, p1) << endl;
+	Client C1(1, "Batman", "Baaat1234", 4000);
+	C1.Display();
+	cout << endl;
 
+	C1.Deposit(1800);
+	C1.Withdraw(2700);
+	cout << endl;
 
+	Client C2(2, "Superman", "Baaat1234", 7000);
+	C2.Display();
+	cout << endl;
+	C1.Transfer_To(900, C2);
+	cout << endl;
+
+	cout << "Client 1 ";
+	C1.Check_Balance();
+
+	cout << "Client 2 ";
+	C2.Check_Balance();
+	cout << "\n===================================\n";
+
+	Client C3(3, "Ronaldo", "Maghawry123", 50000);
+	C3.Display();
+	cout << "\n===================================\n";
+
+	Employee E1(4, "Spiderman", "Spider12345", 200, 9000);
+	E1.Display();
+	cout << "\n===================================\n";
+
+	Admin A1(6, "Ironman", "Iron12345", 600, 8000);
+	A1.Display();
+
+	return 0;
 }
--- Bank_System.h
+++ Bank_System.h
@@ -9,6 +9,7 @@ class Bank_System
 {
 
 };
+
 class Person 
 {
 protected:
@@ -16,30 +17,48 @@ protected:
 	int id;
 	string name;
 	string password;
+	double balance;
 public:
 	//Constructors
 	Person()
 	{
 		id = 0;
+		balance = 0.0;
 	}
-	Person(int id, string name, string password) {
+	Person(int id, string name, string password,double balance) 
+	{
 		this->id = id;
+		this->password = password;
+		this->balance = balance;
 
-		if (Valid_Password(password) && Valid_Name(name)) {
+		//Name Valid
+		if (Valid_Name(name))
 			this->name = name;
+		else
+			cout << "Invalid name format. Name must have a length between 5 and 20." << endl;
+
+		//Pass Valid
+		if (Valid_Password(password))
 			this->password = password;
+		else
+			cout << "Invalid password format. Password must have a length between 8 and 20" << endl;
+
+		//Balance Valid
+		if (balance < 1500)
+		{
+			balance = 1500;
 		}
 	}
 
 	//Name & Password Validation
-	bool Valid_Name(string& name)
+	static bool Valid_Name(string name)
 	{
-		regex pattern("^[A-Z][a-z]{4,19}$");
+		regex pattern("^[A-Z][a-z]{5,20}$");
 
 		return regex_match(name, pattern);
 	}
 
-	bool Valid_Password(string& password)
+	static bool Valid_Password(string password)
 	{
 		regex pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z0-9]{8,20}$");
 
@@ -51,13 +70,24 @@ public:
 	{
 		this->id = id;
 	}
-	void setName(string name) 
+	void setName(string name)
 	{
-		this->name = name;
+		if (Valid_Name(name))
+			this->name = name;
+		else
+			cout << "Invalid name format. Name must have a length between 5 and 20." << endl;
 	}
 	void setPassword(string password)
 	{
-		this->password = password;
+		if (Valid_Password(password))
+			this->password = password;
+		else
+			cout << "Invalid password format. Password must have a length between 8 and 20" << endl;
+	}
+	void setBalance(double balance)
+	{
+		if (balance >= 1500)
+			this->balance = balance;
 	}
 
 	//Getters
@@ -73,6 +103,23 @@ public:
 	{
 		return this->password;
 	}
+	double getBalance()
+	{
+		return balance;
+	}
+
+	virtual void Display()
+	{
+		std::cout << "Person Details : " << endl;
+		std::cout << "ID : " << id << endl;
+		std::cout << "Name : " << name << endl;
+		std::cout << "Balance : " << balance << endl;
+	}
+
+	~Person()
+	{
+		
+	}
 };
 
 class Client : public Person 
@@ -82,58 +129,177 @@ private:
 	double balance;
 public:
 	//Constructors
-	Client() {
-		balance = 1500.0;
+	Client() 
+	{
+		balance = 0.0;
 	}
 	Client(int id, string name, string password, double balance)
-			:Person(id, name, password)
+		:Person(id, name, password, balance)
 	{
 		this->balance = balance;
-	}
-
-	//Setters
-	void setBalance(double balance)
-	{
-		this->balance = balance;
-	}
-	double getBalance() 
-	{
-		return balance;
 	}
 
 	//Deposite
-	double deposite(double amount) 
+	void Deposit(double amount) 
 	{
-		balance += amount;
-		return balance;
+		if (amount > 0) 
+		{
+			balance += amount;
+			cout << "Deposit of " << amount << " successful. New balance : " << balance << endl;
+		}
+		else 
+		{
+			cout << "Invalid deposit amount. Please enter a positive value." << endl;
+		}
 	}
 
 	//Withdraw
-	double withdraw(double amount) 
+	void Withdraw(double amount) 
 	{
-		if (amount <= balance)
+		if (amount > 0 && balance - amount >= 1500) 
+		{
 			balance -= amount;
-		else
-			cout << "Amount Exceeds" << endl;
-		return balance;
+			cout << "Withdrawal of " << amount << " successful. New balance : " << balance << endl;
+		}
+		else 
+		{
+			cout << "Invalid withdrawal amount or insufficient balance." << endl;
+		}
 	}
 
 	//TransferTo
-	double transferTo(double amount, Client& x) 
+	void Transfer_To(double amount, Client& recipient) 
 	{
-		if (amount <= balance) 
+		if (amount > 0 && balance - amount >= 1500) 
 		{
 			balance -= amount;
-			x.deposite(amount);
+			recipient.balance += amount;
+			cout << "Transfer of " << amount << " successful. New balance : " << balance << endl;
 		}
-		else
+		else 
 		{
-			cout << "Amount Exceed" << endl;
+			cout << "Invalid transfer amount or insufficient balance." << endl;
 		}
-		return balance;
 	}
 
-	Person* display() {
+	//Check Balance
+	void Check_Balance() 
+	{
+		cout << "Current balance: " << balance << std::endl;
+	}
+
+	void Display() 
+	{
+		Person::Display();
+	}
+
+	/*Person* display() {
 		return this;
-	};
+	};*/
+
+	~Client()
+	{
+		
+	}
+};
+
+class Employee : public Person
+{
+private:
+	//Attributes
+	double Salary;
+public:
+	//Constructors
+	Employee() 
+	{
+		Salary = 0.0;
+	}
+	Employee(int id, string name, string password, double balance, double Salary)
+		:Person(id, name, password, balance)
+	{
+		this->Salary = Salary;
+
+		if (Salary < 5000)
+		{
+			Salary = 5000;
+		}
+	}
+
+	//Setters
+	void setSalary(double Salary)
+	{
+		if (Salary >= 5000)
+			this->Salary = Salary;
+	}
+
+	//Getters
+	double getSalary()
+	{
+		return this->Salary = Salary;
+	}
+	
+	void Display()
+	{
+		Person::Display();
+		cout << "Salary : " << Salary << endl;
+	}
+	/*Person* display() {
+		return this;
+	};*/
+
+	~Employee()
+	{
+		
+	}
+};
+
+class Admin : public Person
+{
+private:
+	//Attributes
+	double Salary;
+public:
+	//Constructors
+	Admin()
+	{
+		Salary = 0.0;
+	}
+	Admin(int id, string name, string password, double balance, double Salary)
+		:Person(id, name, password, balance)
+	{
+		this->Salary = Salary;
+
+		if (Salary < 5000)
+		{
+			Salary = 5000;
+		}
+	}
+
+	//Setters
+	void setSalary(double Salary)
+	{
+		if (Salary >= 5000)
+			this->Salary = Salary;
+	}
+
+	//Getters
+	double getSalary()
+	{
+		return this->Salary = Salary;
+	}
+
+	void Display()
+	{
+		Person::Display();
+		cout << "Salary : " << Salary << endl;
+	}
+
+	/*Person* display() {
+		return this;
+	};*/
+
+	~Admin()
+	{
+		
+	}
 };
