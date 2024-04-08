33c64d5d46fe3f6ac56b5060b503a046376f5206
MIDO <72321805+Mahmoud-U@users.noreply.github.com>
2024-04-08 15:17:48 +0200

    mods

---
+++ Admin.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <regex>
+#include <exception>
+#include "Employee.h"
+class Admin : public Employee
+{
+public:
+	Admin() {}
+	Admin(int id, string name, string password, double balance, double Salary)
+		:Employee(id, name, password, balance, Salary) {}
+
+	void Display()
+	{
+		Employee::Display();
+	}
+
+	// Methods
+	//void addClient(Client& client)
+	//{
+	//	vector<Client*> clients;			//use pointers to save a lot of copies
+	//	clients.push_back(new Client);
+	//}
+
+	void addClient(Client& client)
+	{
+		vector<Client> clients;
+		clients.push_back(client);
+	}
+
+	Client* searchClient(int id)
+	{
+		
+	}
+
+	/*Person* display() {
+		return this;
+	};*/
+
+	~Admin()
+	{
+
+	}
+};
+
